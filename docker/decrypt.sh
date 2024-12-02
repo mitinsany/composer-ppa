ca0236e0d16ce4f0ac87235ef0b02d45ca5ffb8b
Alexandr <mitinsoft@gmail.com>
2024-12-03 00:49:16 +0200

    Update decrypt.sh

--- docker/decrypt.sh
+++ docker/decrypt.sh
@@ -2,13 +2,10 @@
 
 set -euxo pipefail
 
-GPG_TTY=$(tty)
-export GPG_TTY
-
 openssl aes-256-cbc \
     -K "$ENCRYPTED_KEY" \
     -iv "$ENCRYPTED_IV" \
     -in 0xCF6EC707.asc.enc \
     -out 0xCF6EC707.asc -d
 
-gpg --import 0xCF6EC707.asc
+gpg --no-tty --import 0xCF6EC707.asc
